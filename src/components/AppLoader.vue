7a33af719ba4cede8920fbab3c3cb06299d63c25
carloeusebi <carloeusebi@gmail.com>
2023-09-29 10:17:42 +0200

    Implemented Loader

---
+++ src/components/AppLoader.vue
@@ -0,0 +1,25 @@
+<script lang="ts" setup>
+import { useLoaderStore } from '@/stores';
+
+const loader = useLoaderStore();
+</script>
+
+<template>
+  <div class="layover" v-if="loader.isLoading">
+    <div class="h1 text-white">Loading...</div>
+  </div>
+</template>
+
+<style scoped>
+.layover {
+  position: fixed;
+  inset: 0;
+  z-index: 1;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  background-color: rgba(0, 0, 0, 0.6);
+}
+</style>
